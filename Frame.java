2fb10ff7825b3dfa8512df3d2581a0e1bd37bcf2
musharofahmed1 <musharofahmed11@gmail.com>
2023-11-17 21:36:12 -0600

    JFrame

--- Frame.java
+++ Frame.java
@@ -1,17 +1,23 @@
-//package BroCode;
-
 import java.awt.Color;
-
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Frame {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setTitle("Bro Code");
-        frame.setSize(400, 300); // Set the size of the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the application when the frame is closed
-        frame.setVisible(true); // Make the frame visible
 
-        frame.getContentPane().setBackground(Color.PINK);
-    }
+ public static void main(String[] args) {
+
+  // JFrame = a GUI window to add components to
+  
+  JFrame frame = new JFrame(); //creates a frame
+  frame.setTitle("JFrame title goes here"); //sets title of frame
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit from application
+  frame.setResizable(false); //prevent frame from being resized
+  frame.setSize(420,420); //sets the x-dimension, and y-dimension of frame
+  frame.setVisible(true); //make frame visible
+  
+  ImageIcon image = new ImageIcon("correct.png"); //create an ImageIcon
+  frame.setIconImage(image.getImage()); //change icon of frame
+  frame.getContentPane().setBackground(new Color(0x123456)); //change color of background
+    
+ }
 }
